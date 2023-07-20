ff943a53de82b58972158d2b3c67baf4c5a70fe9
YCY <app97204@gmail.com>
2023-07-20 13:08:12 +0800

    Initial Commit

---
+++ Game1A2B Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Game1A2B Watch App
+//
+//  Created by Cheng Yan Yang on 2023/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Game1A2B Watch App/Game1A2BApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game1A2BApp.swift
+//  Game1A2B Watch App
+//
+//  Created by Cheng Yan Yang on 2023/7/20.
+//
+
+import SwiftUI
+
+@main
+struct Game1A2B_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
